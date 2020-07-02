f6345904e6cd09669c270e40f0537b01406a188c
gaby2327 <gabriela.uv@outlook.com>
2020-07-02 12:43:00 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# OugiMusic
+ Reproductor de música para dispositivos móviles
---
+++ app/src/main/java/com/example/ougimusic/InicioReproductor.kt
@@ -0,0 +1,11 @@
+package com.example.ougimusic
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class InicioReproductor : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_inicio_reproductor)
+    }
+}
