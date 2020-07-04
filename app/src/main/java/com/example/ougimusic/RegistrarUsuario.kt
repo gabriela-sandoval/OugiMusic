362951a6372c1d9eecfebe820c8793846c493c85
gaby2327 <gabriela.uv@outlook.com>
2020-07-03 20:41:02 -0500

    Creación de ventana para registro de usuarios

--- app/src/main/java/com/example/ougimusic/RegistrarUsuario.kt
+++ app/src/main/java/com/example/ougimusic/RegistrarUsuario.kt
@@ -1,11 +1,19 @@
 package com.example.ougimusic
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_registrar_usuario.*
 
 class RegistrarUsuario : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registrar_usuario)
+
+        buttonRegresar.setOnClickListener {
+            val intent: Intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
