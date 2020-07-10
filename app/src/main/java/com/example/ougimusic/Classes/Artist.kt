afdf38c80f27c04005d141d909d9d69878f28b00
jarturo <jarturogl05@gmail.com>
2020-07-09 20:48:59 -0500

    clases

---
+++ app/src/main/java/com/example/ougimusic/Classes/Artist.kt
@@ -0,0 +1,9 @@
+package com.example.ougimusic.Classes
+
+public class Artist {
+    var name : String? = null
+    var description : String? = null
+    var debutYear : String? = null
+    var urlImage : String?  = null
+
+}
---
+++ app/src/main/java/com/example/ougimusic/Classes/Playlist.kt
@@ -0,0 +1,4 @@
+package com.example.ougimusic.Classes
+
+class Playlist {
+}
