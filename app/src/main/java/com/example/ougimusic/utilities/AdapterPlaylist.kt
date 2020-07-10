3b5a9fe8a0d8269ac68a3c90b014be02c9460f1d
davisbd100 <davisbd100@gmail.com>
2020-07-10 17:33:37 -0500

    Muestra de canciones en playlist

--- app/src/main/java/com/example/ougimusic/utilities/AdapterPlaylist.kt
+++ app/src/main/java/com/example/ougimusic/utilities/AdapterPlaylist.kt
@@ -1,5 +1,6 @@
 package com.example.ougimusic.utilities
 
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -7,10 +8,10 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.Adapter
+import com.example.ougimusic.Canciones
 import com.example.ougimusic.R
 
 class AdapterPlaylist(var list: List<PlaylistData.Playlist>) : Adapter<AdapterPlaylist.PlayListViewHolder>(){
-
 
     class PlayListViewHolder(view:View): RecyclerView.ViewHolder(view){
         fun bindItem(data: PlaylistData.Playlist){
@@ -18,7 +19,11 @@ class AdapterPlaylist(var list: List<PlaylistData.Playlist>) : Adapter<AdapterPl
             name.text = data.name
 
             itemView.setOnClickListener{
-                Toast.makeText(itemView.context, "Id de este: ${data._id}", Toast.LENGTH_SHORT).show()
+                val intent = Intent(it.context, Canciones::class.java)
+                intent.putExtra("playlistName", data.name)
+                intent.putExtra("idPlaylist", data.name)
+                intent.putStringArrayListExtra("songList", data.songs)
+                it.context.startActivity(intent)
             }
         }
     }
--- app/src/main/java/com/example/ougimusic/utilities/PlaylistData.kt
+++ app/src/main/java/com/example/ougimusic/utilities/PlaylistData.kt
@@ -1,16 +1,37 @@
 package com.example.ougimusic.utilities
 
-class PlaylistData {
+import android.icu.text.CaseMap
+import android.os.Parcelable
+import com.example.ougimusic.Albumes
+import java.io.Serializable
+
+class PlaylistData : Serializable{
 
     data class Response(
         val status: String,
         val data: List<Playlist>
     )
 
-    data class Playlist(
+    data class Playlist (
         val _id: String,
-        val songs: List<String>,
+        val songs: ArrayList<String>,
         val user: String,
         val name: String
+    )
+    data class SongResponse(
+        val _id: String,
+        val data: Song
+    )
+    data class Song(
+        val title: String,
+        val number: Double,
+        val album: String,
+        val albumId: String,
+        val artist: String,
+        val artistId: String,
+        val genre: String,
+        val year: String,
+        val urlStreaming: String,
+        val urlImage: String
     )
 }
