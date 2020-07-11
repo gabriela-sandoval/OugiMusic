b9580deb608389538cda2259c9829387e71a6e45
davisbd100 <davisbd100@gmail.com>
2020-07-11 12:30:39 -0500

    Clase Playlist.kt

--- app/src/main/java/com/example/ougimusic/Classes/Playlist.kt
+++ app/src/main/java/com/example/ougimusic/Classes/Playlist.kt
@@ -1,4 +1,8 @@
 package com.example.ougimusic.Classes
 
 class Playlist {
+    val _id: String? = null
+    val songs: ArrayList<String>? = null
+    val user: String? = null
+    val name: String? = null
 }
