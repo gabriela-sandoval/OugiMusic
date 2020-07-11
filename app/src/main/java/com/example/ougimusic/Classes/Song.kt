32a101e4f024eb81f1adc9e20a21b1e19a7b312e
davisbd100 <davisbd100@gmail.com>
2020-07-11 12:33:44 -0500

    Agregado de atributos a clase Song

--- app/src/main/java/com/example/ougimusic/Classes/Song.kt
+++ app/src/main/java/com/example/ougimusic/Classes/Song.kt
@@ -6,7 +6,9 @@ public class Song : Serializable  {
     var title: String? = null
     val number = 0
     var artist: String? = null
+    var artistId: String? = null
     var album: String? = null
+    var albumId: String? = null
     var urlStreaming: String? = null
     val genre: String? = null
     val year: String? = null
