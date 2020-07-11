f7bc7c2b547aca68369394023ffe6a1f2d535679
jarturo <jarturogl05@gmail.com>
2020-07-10 22:24:55 -0500

    manejo de errores si no existe cola

--- app/src/main/java/com/example/ougimusic/InicioReproductor.kt
+++ app/src/main/java/com/example/ougimusic/InicioReproductor.kt
@@ -6,6 +6,7 @@ import android.media.MediaPlayer
 import android.os.Bundle
 import android.os.Handler
 import android.os.Message
+import android.os.PersistableBundle
 import android.view.MenuItem
 import android.view.View
 import android.widget.*
@@ -42,13 +43,9 @@ class InicioReproductor : AppCompatActivity(), NavigationView.OnNavigationItemSe
     var song: Song? = null
     var queue:Queue? = null
 
-
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_inicio_reproductor)
-
         barraProgreso = findViewById(R.id.barraProgreso)
         botonPlay = findViewById(R.id.botonPlay)
         botonAnterior = findViewById(R.id.botonAnterior)
@@ -58,89 +55,68 @@ class InicioReproductor : AppCompatActivity(), NavigationView.OnNavigationItemSe
         textViewNombreCanción = findViewById(R.id.textViewNombreCanción)
         textViewNombreArtista = findViewById(R.id.textViewNombreArtista)
         songImage = findViewById(R.id.songImage)
-
         toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
-
         drawerLayout = findViewById(R.id.drawer_layout)
         navView = findViewById(R.id.nav_view)
-
         val toggle = ActionBarDrawerToggle(
             this, drawerLayout, toolbar, 0, 0
         )
         drawerLayout.addDrawerListener(toggle)
         toggle.syncState()
-
         navView.setNavigationItemSelectedListener(this)
 
-
-
-
         queue = intent.getSerializableExtra("Current_List") as? Queue
-        position = queue?.currentSongPosition!!
-        song = queue?.currentList?.get(this.position)
 
+        if(queue != null){
+            position = queue?.currentSongPosition!!
+            song = queue?.currentList?.get(this.position)
 
-        mp = MediaPlayer()
-        playSong(urlStreaming = song?.urlStreaming)
-        updateSongInfo(song?.title, song?.artist)
-        updateSongArt(song)
+            mp = MediaPlayer()
+            playSong(urlStreaming = song?.urlStreaming)
+            updateSongInfo(song?.title, song?.artist)
+            updateSongArt(song)
 
+            barraProgreso.max = totalTime
+            barraProgreso.setOnSeekBarChangeListener(
+                object : SeekBar.OnSeekBarChangeListener{
+                    override fun onProgressChanged(
+                        seekBar: SeekBar?,
+                        progress: Int,
+                        fromUser: Boolean
+                    ) {
+                        if(fromUser){
+                            mp!!.seekTo(progress)
+                        }
+                    }
 
+                    override fun onStartTrackingTouch(p0: SeekBar?) {
+                    }
 
-
-        barraProgreso.max = totalTime
-        barraProgreso.setOnSeekBarChangeListener(
-            object : SeekBar.OnSeekBarChangeListener{
-                override fun onProgressChanged(
-                    seekBar: SeekBar?,
-                    progress: Int,
-                    fromUser: Boolean
-                ) {
-                    if(fromUser){
-                        mp!!.seekTo(progress)
+                    override fun onStopTrackingTouch(p0: SeekBar?) {
                     }
                 }
+            )
 
-                override fun onStartTrackingTouch(p0: SeekBar?) {
+            Thread(Runnable {
+                while (mp != null) {
+                    try {
+                        var msg = Message()
+                        msg.what = mp!!.currentPosition
+                        handler.sendMessage(msg)
+                        Thread.sleep(1000)
+                    } catch (e: InterruptedException) {
+                    }
                 }
-
-                override fun onStopTrackingTouch(p0: SeekBar?) {
-                }
-            }
-        )
-
-        Thread(Runnable {
-            while (mp != null) {
-                try {
-                    var msg = Message()
-                    msg.what = mp!!.currentPosition
-                    handler.sendMessage(msg)
-                    Thread.sleep(1000)
-                } catch (e: InterruptedException) {
-                }
-            }
-        }).start()
-
-
-    }
-
-    fun updateSongArt(song: Song?){
-        Picasso.get().load(song?.urlImage).into(songImage);
-
-    }
-
-    fun playSong(urlStreaming: String?) {
-
-        try{
-            mp!!.setDataSource(urlStreaming)
-            mp!!.prepare()
-            totalTime = mp!!.duration
-            mp!!.start()
-        }catch(e: IOException){
-            Toast.makeText(this, "mp3 no encontrado", Toast.LENGTH_SHORT).show();
+            }).start()
         }
+
     }
+
+    override fun onSaveInstanceState(outState: Bundle, outPersistentState: PersistableBundle) {
+        super.onSaveInstanceState(outState, outPersistentState)
+    }
+
     override fun onBackPressed() {
         val failure = AlertDialog.Builder(this)
         failure.setTitle("¿Desea Salir?")
@@ -155,6 +131,27 @@ class InicioReproductor : AppCompatActivity(), NavigationView.OnNavigationItemSe
         }
         failure.show()
     }
+
+    fun updateSongArt(song: Song?){
+        Picasso.get().load(song?.urlImage).into(songImage);
+    }
+
+    fun playSong(urlStreaming: String?) {
+
+
+        if (queue != null){
+            try{
+                mp!!.setDataSource(urlStreaming)
+                mp!!.prepare()
+                totalTime = mp!!.duration
+                mp!!.start()
+            }catch(e: IOException){
+                Toast.makeText(this, "mp3 no encontrado", Toast.LENGTH_SHORT).show();
+            }
+        }
+
+    }
+
     fun updateSongInfo(title: String?, artist: String?) {
         textViewNombreCanción.text = title
         textViewNombreArtista.text = artist
@@ -193,64 +190,66 @@ class InicioReproductor : AppCompatActivity(), NavigationView.OnNavigationItemSe
 
     fun playBtnClick(v: View) {
 
-        if (this.mp?.isPlaying!!) {
-            // Stop
-            this.mp!!.pause()
-            botonPlay.setBackgroundResource(R.drawable.play_button)
-        } else {
-            // Start
-            this.mp!!.start()
-            botonPlay.setBackgroundResource(R.drawable.pause)
+        if(queue != null) {
+
+            if (this.mp?.isPlaying!!) {
+                // Stop
+                this.mp!!.pause()
+                botonPlay.setBackgroundResource(R.drawable.play_button)
+            } else {
+                // Start
+                this.mp!!.start()
+                botonPlay.setBackgroundResource(R.drawable.pause)
+            }
         }
+
+
     }
 
 
     fun nextSong(v: View){
 
-        var queueSize = queue?.currentList?.size
-        if( position+1 < queueSize!!) {
-            position++
-            var nextSong = queue?.currentList?.get(position)
-            song = nextSong
-            mp?.reset()
-            updateSongInfo(song?.title, song?.artist)
-            updateSongArt(song)
-            playSong(song?.urlStreaming)
-        }else{
-            Toast.makeText(this, "No hay más canciones", Toast.LENGTH_SHORT).show();
-
+        if(queue != null){
+            var queueSize = queue?.currentList?.size
+            if( position+1 < queueSize!!) {
+                position++
+                var nextSong = queue?.currentList?.get(position)
+                song = nextSong
+                mp?.reset()
+                updateSongInfo(song?.title, song?.artist)
+                updateSongArt(song)
+                playSong(song?.urlStreaming)
+            }else{
+                Toast.makeText(this, "No hay más canciones", Toast.LENGTH_SHORT).show();
+            }
         }
-
     }
 
     fun previousSong(v: View){
 
-        var queueSize = queue?.currentList?.size
-        if( position+1 < queueSize!! && position != 0) {
-            position--
-            var nextSong = queue?.currentList?.get(position)
-            song = nextSong
-            mp?.reset()
-            updateSongInfo(song?.title, song?.artist)
-            updateSongArt(song)
-            playSong(song?.urlStreaming)
-        }else {
+        if(queue != null) {
+            var queueSize = queue?.currentList?.size
+            if( position+1 < queueSize!! && position != 0) {
+                position--
+                var nextSong = queue?.currentList?.get(position)
+                song = nextSong
+                mp?.reset()
+                updateSongInfo(song?.title, song?.artist)
+                updateSongArt(song)
+                playSong(song?.urlStreaming)
+            }else {
 
-
-            Toast.makeText(this, "No hay más canciones", Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, "No hay más canciones", Toast.LENGTH_SHORT).show();
+            }
         }
+
     }
-
-
-
-
-
 
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
             R.id.nav_inicio -> {
-                mp?.reset()
+                mp?.stop()
                 val intent: Intent = Intent(this, InicioReproductor::class.java)
                 startActivity(intent)
                 finish()
