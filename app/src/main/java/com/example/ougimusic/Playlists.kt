424b3d355870d0d5b8729cae9e5a3fd242785735
davisbd100 <davisbd100@gmail.com>
2020-07-12 02:31:19 -0500

    Creacion de lista de reproduccion

--- app/src/main/java/com/example/ougimusic/Playlists.kt
+++ app/src/main/java/com/example/ougimusic/Playlists.kt
@@ -1,6 +1,8 @@
 package com.example.ougimusic
 
+import android.app.AlertDialog
 import android.content.Context
+import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
@@ -8,6 +10,7 @@ import android.view.MenuItem
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.ougimusic.Classes.Playlist
 import com.example.ougimusic.utilities.AdapterPlaylist
 import com.example.ougimusic.utilities.ContextVariables
 import com.example.ougimusic.utilities.ResponseMessages
@@ -15,6 +18,8 @@ import com.google.gson.GsonBuilder
 import kotlinx.android.synthetic.main.activity_playlists.*
 import kotlinx.android.synthetic.main.activity_registrar_usuario.*
 import kotlinx.android.synthetic.main.activity_registrar_usuario.buttonRegresar
+import kotlinx.android.synthetic.main.layout_new_playlist.*
+import kotlinx.android.synthetic.main.layout_new_playlist.view.*
 import okhttp3.*
 import okhttp3.MediaType.Companion.toMediaTypeOrNull
 import okhttp3.RequestBody.Companion.toRequestBody
@@ -24,25 +29,45 @@ class Playlists : AppCompatActivity() {
     private val client = OkHttpClient()
     private val gson = GsonBuilder().create()
     private val global = ContextVariables()
+    private val playlistList = mutableListOf<Playlist>()
+    private val adapter = AdapterPlaylist(playlistList)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_playlists)
+        val recycler = findViewById<RecyclerView>(R.id.recyclePlaylist)
+        recycler.layoutManager = LinearLayoutManager(parent, RecyclerView.VERTICAL, false)
+        recycler.adapter = adapter
         run{
-            GetSongsPlaylist()
+            GetPlaylist()
         }
         buttonRegresar.setOnClickListener {
             finish()
         }
         buttonNewList.setOnClickListener{
-            val listName = "newlist1"
-            run {
-                CreateNewPlaylist(listName)
-            }
+            OpenDialog()
         }
     }
 
-    fun GetSongsPlaylist(){
+
+    fun OpenDialog(){
+        val builder = AlertDialog.Builder(this)
+        val inflater = this.layoutInflater
+        val view = inflater?.inflate(R.layout.layout_new_playlist, null)
+        builder.setView(view)
+            .setTitle("Crear nueva lista")
+            .setNegativeButton("Cancelar", DialogInterface.OnClickListener { dialog, which ->
+
+            })
+            .setPositiveButton("Crear", DialogInterface.OnClickListener { dialog, which ->
+                run{
+                    CreatePlaylist(view.playlistName.text.toString())
+                }
+            })
+        builder.create().show()
+    }
+
+    fun GetPlaylist(){
         val userPreferences = getSharedPreferences("user", Context.MODE_PRIVATE)
         val username = userPreferences.getString("username", "")
         val json = """
@@ -72,16 +97,15 @@ class Playlists : AppCompatActivity() {
                     val bodyResponse = response.body!!.string()
                     val jsonResponse = gson.fromJson(bodyResponse, ResponseMessages.PlaylistResponse::class.java)
                     runOnUiThread{
-                        val recycler = findViewById<RecyclerView>(R.id.recyclePlaylist)
-                        recycler.layoutManager = LinearLayoutManager(parent, RecyclerView.VERTICAL, false)
-                        val adapter = AdapterPlaylist(jsonResponse.data)
-                        recycler.adapter = adapter
+                        playlistList.clear()
+                        playlistList.addAll(jsonResponse.data)
+                        adapter.notifyDataSetChanged()
                     }
                 }
             }
         })
     }
-    fun CreateNewPlaylist(listName:String){
+    fun CreatePlaylist(listName: String){
         val userPreferences = getSharedPreferences("user", Context.MODE_PRIVATE)
         val username = userPreferences.getString("username", "")
         val json = """
@@ -98,18 +122,19 @@ class Playlists : AppCompatActivity() {
         client.newCall(request).enqueue(object : Callback{
             override fun onFailure(call: Call, e: IOException) {
                 runOnUiThread{
-                    Toast.makeText(applicationContext, "No se pudo crear la lista: $listName", Toast.LENGTH_SHORT).show()
+                    Toast.makeText(applicationContext, "No se pudo crear la lista de reproduccion", Toast.LENGTH_SHORT).show()
                 }
             }
 
             override fun onResponse(call: Call, response: Response) {
                 if(!response.isSuccessful){
-                        runOnUiThread{
-                            Toast.makeText(applicationContext, "Existe un error de tipo: ${response.message}", Toast.LENGTH_SHORT).show()
-                        }
+                    runOnUiThread{
+                        Toast.makeText(applicationContext, "Existe un error de tipo: ${response.message}", Toast.LENGTH_SHORT).show()
+                    }
                 }else{
                     runOnUiThread{
                         Toast.makeText(applicationContext, "Lista creada", Toast.LENGTH_SHORT).show()
+                        GetPlaylist()
                     }
                 }
             }
