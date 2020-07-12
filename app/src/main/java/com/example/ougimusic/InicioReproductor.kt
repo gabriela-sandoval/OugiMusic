9e6ee34d48d21f07bb51dd4d9811e69486d73452
jarturo <jarturogl05@gmail.com>
2020-07-12 14:58:50 -0500

    mostrar todas las canciones

--- app/src/main/java/com/example/ougimusic/InicioReproductor.kt
+++ app/src/main/java/com/example/ougimusic/InicioReproductor.kt
@@ -300,7 +300,7 @@ class InicioReproductor : AppCompatActivity(), NavigationView.OnNavigationItemSe
             }
             R.id.nav_canciones -> {
                 mp?.stop()
-                val intent = Intent(this, Canciones::class.java)
+                val intent = Intent(this, CancionesPrincipal::class.java)
                 startActivity(intent)
                 finish()
             }
