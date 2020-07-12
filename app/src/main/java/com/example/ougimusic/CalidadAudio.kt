6ee1d322c29164afee0e89067d340b7ba341519a
gaby2327 <gabriela.uv@outlook.com>
2020-07-12 13:53:34 -0500

    Ventana Calidad de audio

---
+++ app/src/main/java/com/example/ougimusic/CalidadAudio.kt
@@ -0,0 +1,41 @@
+package com.example.ougimusic
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Switch
+import kotlinx.android.synthetic.main.activity_registrar_usuario.*
+
+class CalidadAudio : AppCompatActivity() {
+    internal lateinit var switchAlta: Switch
+    internal lateinit var switchMedia: Switch
+    internal lateinit var switchBaja: Switch
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_calidad_audio)
+
+        buttonRegresar.setOnClickListener {
+            finish()
+        }
+
+        switchAlta = findViewById<View>(R.id.switchCalidadAlta) as Switch
+        switchMedia = findViewById<View>(R.id.switchCalidadMedia) as Switch
+        switchBaja = findViewById<View>(R.id.switchCalidadBaja) as Switch
+
+        switchAlta.setOnClickListener{
+            if(switchAlta.isChecked)
+            {
+                switchMedia.isChecked = false
+                switchBaja.isChecked = false
+            } else if(switchMedia.isChecked) {
+                switchAlta.isChecked = false
+                switchBaja.isChecked = false
+            } else if (switchBaja.isChecked)
+            {
+                switchMedia.isChecked = false
+                switchAlta.isChecked = false
+            }
+        }
+    }
+}
